c5b757cb360721aaa37257946e32421af88f9d6a
chen q <chenqin41@hotmail.com>
2018-08-07 16:49:30 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>
+        <mheader></mheader>
+      </el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+  import Header from '@/components/Header'
+export default {
+  name: 'App',
+  components: {
+    'mheader': Header
+  }
+}
+</script>
+
+<style scoped>
+.el-header {
+  padding: 0;
+  color: #fff;
+  line-height: 80px;
+}
+</style>
---
+++ src/components/Header/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <el-row class="header">
+    <el-col :span="4">
+    <div class="head-wrap">Element</div>
+    </el-col>
+    <el-col :span="16">
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+        <el-menu-item index="1">处理中心</el-menu-item>
+        <el-submenu index="2">
+          <template slot="title">我的工作台</template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
+          <el-submenu index="2-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="2-4-1">选项1</el-menu-item>
+            <el-menu-item index="2-4-2">选项2</el-menu-item>
+            <el-menu-item index="2-4-3">选项3</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+      </el-menu>
+    </el-col>
+    <el-col :span="4"></el-col>
+  </el-row>
+
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+<style>
+  .header {
+    background-color: #545c64;
+  }
+  .el-menu--horizontal>.el-menu-item {
+    height: 80px;
+    line-height: 80px;
+  }
+  .el-menu--horizontal>.el-submenu .el-submenu__title {
+    height: 80px;
+    line-height: 80px;
+  }
+</style>
